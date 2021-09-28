6c6d199528a5d6db14dfa9a6a2158ca4539eb14e
gueilinwang <cool4003213@gmail.com>
2021-09-28 16:33:02 +0800

    添加code splittingtting功能
    配置splitChunksPlugin

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-// import "./scss/style.scss"
-// import "@babel/polyfill"
-import counter from "./components/counter"
-import number from "./components/number"
-counter()
-number()
-
-const arr = [1, 2, 3, 4]
-console.log(arr.includes(1))
-if (module.hot) {
-  module.hot.accept("./components/number", function () {
-    document.body.removeChild(document.getElementById("number"))
-    number()
-  })
+function getComponent() {
+  return import(/* webpackChunkName:"lodash" */ "lodash").then(
+    ({ default: _ }) => {
+      const element = document.createElement("div")
+      element.innerHTML = _.join(["Samuel", "Wang"], "-")
+      return element
+    }
+  )
 }
+
+getComponent().then((element) => {
+  document.body.appendChild(element)
+})
+// import _ from "lodash"
+// const element = document.createElement("div")
+// element.innerHTML = _.join(["Samuel", "Wang"], "-")
+// document.body.appendChild(element)
--- webpack.common.js
+++ webpack.common.js
@@ -56,6 +56,29 @@ module.exports = {
     new CleanWebpackPlugin(),
   ],
   optimization: {
-    usedExports: true,
+    usedExports: true, //tree shakings
+    //代碼分割
+    splitChunks: {
+      chunks: "all", //'async:只對非同步代碼進行分割;initial只對同步代碼進行分割'
+      minSize: 20000, // >20kb才做分割
+      minRemainingSize: 0,
+      minChunks: 1, //當一個模塊至少被引用一次時會被當成chunk
+      maxAsyncRequests: 30, //同時加載的模塊數，最多30個
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true, //如果一個模塊之前已經被打包分割過了,則不會再去做額外的分割
+          filename: "vendor.js",
+        },
+        default: {
+          minChunks: 1,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
   },
 }
